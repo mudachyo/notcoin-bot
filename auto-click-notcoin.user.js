c8be556a0b286419f1fdeaa83b4f7958bdba2791
Daniil Voroshin <kotanoff@raikage.ru>
2024-03-18 04:45:00 +0300

    Update auto-click-notcoin.user.js

--- auto-click-notcoin.user.js
+++ auto-click-notcoin.user.js
@@ -1,36 +1,39 @@
 // ==UserScript==
 // @name         Auto-Click NOTCOIN
 // @namespace    Violentmonkey Scripts
-// @match       https://clicker.joincommunity.xyz/clicker*
-// @version     1.0
-// @author      mudachyo
-// @description 09.01.2024, 01:15:41
+// @match        https://clicker.joincommunity.xyz/clicker*
+// @version      1.1
+// @author       mudachyo
+// @description  18.03.2024, 04:40:59
 // ==/UserScript==
 
 (function() {
     'use strict';
 
+    // Задержка перед началом клика
     setTimeout(click, 3000);
 
     async function click() {
-        let globalscore = 1000;
-        let countclicks = 34;
-        let cc = document.querySelectorAll('div[class^="_notcoin"]');
-        let scoreElement = document.querySelector('div[class^="_scoreCurrent"]');
+        const globalScore = 1000; // Минимальное кол-во энергии для клика
+        const clickCount = 34; // Количество кликов
+        const coinElements = document.querySelectorAll('div[class^="_notcoin"]');
+        const scoreElement = document.querySelector('div[class^="_scoreCurrent"]');
         let score = parseInt(scoreElement.textContent);
 
         try {
-            let imrocket = document.querySelectorAll('img[class^="_root"]');
-            imrocket[0][Object.keys(imrocket[0])[1]].onClick();
+            // Попытка нажать на ракету, если она есть
+            const rocketElements = document.querySelectorAll('img[class^="_root"]');
+            rocketElements[0][Object.keys(rocketElements[0])[1]].onClick();
         } catch (error) {}
 
-        for (let step = 0; step < countclicks; step++) {
+        for (let step = 0; step < clickCount; step++) {
             score = parseInt(scoreElement.textContent);
 
-            if (score > globalscore) {
+            if (score > globalScore) {
                 try {
+                    // Клик на монету
                     await new Promise((resolve) => {
-                        cc[0][Object.keys(cc[0])[1]].onTouchStart('');
+                        coinElements[0][Object.keys(coinElements[0])[1]].onTouchStart('');
                         setTimeout(resolve, 100);
                     });
                 } catch (error) {}
@@ -40,5 +43,6 @@
         }
     }
 
+    // Повторять клик каждые 500 миллисекунд
     setInterval(click, 500);
 })();
