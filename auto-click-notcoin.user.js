5a5d2c049df8d39e84801bbe84cd138bc861a32a
Daniil Voroshin <kotanoff@raikage.ru>
2024-03-20 16:10:47 +0300

    👆 refactor: rename clickCount to countclicks

--- auto-click-notcoin.user.js
+++ auto-click-notcoin.user.js
@@ -15,7 +15,7 @@
 
     async function click() {
         const globalScore = 1000; // Минимальное кол-во энергии для клика
-        const clickCount = 34; // Количество кликов
+        const countclicks = 34; // Количество кликов
         const coinElements = document.querySelectorAll('div[class^="_notcoin"]');
         const scoreElement = document.querySelector('div[class^="_scoreCurrent"]');
         let score = parseInt(scoreElement.textContent);
@@ -26,7 +26,7 @@
             rocketElements[0][Object.keys(rocketElements[0])[1]].onClick();
         } catch (error) {}
 
-        for (let step = 0; step < clickCount; step++) {
+        for (let step = 0; step < countclicks; step++) {
             score = parseInt(scoreElement.textContent);
 
             if (score > globalScore) {
